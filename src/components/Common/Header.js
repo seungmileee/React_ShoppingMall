ca1e5fa2e40b40a8c802e87683546a1d974bbcd6
seungmileee <dltmdal0928@naver.com>
2022-11-17 15:53:28 +0900

    feat: header 완성, 로그인 페이지/로그아웃 페이지 ui 완성

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { AiFillShop } from "react-icons/ai";
+import { AiFillShop, AiOutlineShoppingCart } from "react-icons/ai";
 
 function Header() {
   const navigate = useNavigate();
 
   return (
-    <header className="w-full flex p-4 border-b">
-      <div className="flex items-center">
+    <header className="w-full flex justify-between p-4 border-b font-bold">
+      <section className="flex items-center text-orange-400">
         <button onClick={() => navigate("/")} className="mr-2 text-4xl">
           <AiFillShop />
         </button>
-        <h1 className="font-bold text-2xl">Shoppy</h1>
-      </div>
+        <h1 className="text-2xl">Shoppy</h1>
+      </section>
+      <section className="flex items-center">
+        <button onClick={() => navigate("/products")} className="mr-5">
+          Products
+        </button>
+        <button onClick={() => navigate("/cart")} className="mr-5 text-3xl">
+          <AiOutlineShoppingCart />
+        </button>
+        <button
+          onClick={() => navigate("/signin")}
+          className="bg-orange-400 text-white px-3 py-1.5 border-solid border-2 border-orange-400 ease-in duration-200 hover:bg-white hover:text-orange-400"
+        >
+          Login
+        </button>
+      </section>
     </header>
   );
 }
